1212b120c5e4cea66a66c37609b3ec248931d98d
Kacper Dumienski <kacper.dumienski@microsoft.wsei.edu.pl>
2022-03-13 09:30:45 +0100

    progress

--- lab1/proj2/src/com/company/Main2.java
+++ lab1/proj2/src/com/company/Main2.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Co wpisac do pliku?");
 
+        var input = System.console().readLine();
+
+//        var file = new File("output.txt");
+//
+//        file.createNewFile();
+
+        try (var myWriter = new FileWriter("filename.txt"))
+        {
+            myWriter.write(input);
+        }
+
+        System.out.println("wpisano");
     }
 }
